e549965d00e307621ece82449146f4aff0091f46
Дмитрий Дубовик <dim-dubovik@yandex.ru>
2022-04-22 12:46:58 +0300

    feat: add search

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Search.scss';
 
 export const Search = () => {
+  const [text, setText] = useState('');
+
+  const handleText = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setText(e.target.value);
+  };
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(text);
+  };
+
   return (
-    <div>
-      <input type='text' />
-    </div>
+    <form className='search' onSubmit={handleSubmit}>
+      <input type='text' className='search__text' value={text} onChange={handleText} />
+      <input type='submit' hidden />
+    </form>
   );
 };

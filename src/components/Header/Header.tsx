9fe3e652a7e72b5835adc395b1cd3aef895f0e7c
Дмитрий Дубовик <dim-dubovik@yandex.ru>
2022-04-22 07:26:10 +0300

    feat: add search

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Search } from '../Search/Search';
 import './Header.scss';
 
 export const Header = () => {
-  return <div className='header'>Header-Search</div>;
+  return (
+    <div className='header'>
+      <Link to='' className='header__logo' />
+      <Search />
+    </div>
+  );
 };
---
+++ src/components/Search/Search.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Search.scss';
+
+export const Search = () => {
+  return (
+    <div>
+      <input type='text' />
+    </div>
+  );
+};

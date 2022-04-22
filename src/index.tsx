d07674fddb8d1d1a2e7033f2edb92944d6c6276a
Дмитрий Дубовик <dim-dubovik@yandex.ru>
2022-04-22 03:35:05 +0300

    feat: remove index.scss

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
 import { App } from './components/App/App';
 // import { store } from './redux/store';
 // import { Provider } from 'react-redux';

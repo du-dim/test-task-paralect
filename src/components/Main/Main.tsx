9704d3f4a7084607206d9cb88269b701f4b9fa9f
du-dim <dim-dubovik@yandex.ru>
2022-04-23 22:31:14 +0300

    refactor: main routes

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
-import { UseRoutes } from '../UseRoutes';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { Start } from '../Start/Start';
+import { NoUser } from '../NoUser/NoUser';
+import { User } from '../User/User';
 import './Main.scss';
 
 export const Main = () => {
   return (
     <div className='main'>
-      <UseRoutes />
+      <Routes>
+        <Route path='/' element={<Start />} />
+        <Route path='/nouser' element={<NoUser />} />
+        <Route path='/user' element={<User />} />
+        <Route path='/*' element={<Navigate to='/' />} />
+      </Routes>
     </div>
   );
 };

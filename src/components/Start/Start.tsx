8f8c4cd97403ea7ca0f8898687ad3c88703efbe5
du-dim <dim-dubovik@yandex.ru>
2022-04-23 23:19:11 +0300

    feat: add style start

--- src/components/Start/Start.tsx
+++ src/components/Start/Start.tsx
@@ -1,5 +1,13 @@
 import React from 'react';
+import './Start.scss';
 
 export const Start = () => {
-  return <div>Start</div>;
+  return (
+    <div className='start'>
+      <div className='start__icon'>
+        <img className='start__icon_img' src='./assets/search.svg' alt='search' />
+      </div>
+      <div className='start__text'>Start with searching a GitHub user</div>
+    </div>
+  );
 };

97949fdf2d4743b22604acda3c0f9d02608d3cc8
du-dim <dim-dubovik@yandex.ru>
2022-04-23 23:30:09 +0300

    feat: add style nouser

--- src/components/NoUser/NoUser.tsx
+++ src/components/NoUser/NoUser.tsx
@@ -1,5 +1,13 @@
 import React from 'react';
+import './NoUser.scss';
 
 export const NoUser = () => {
-  return <div>NoUser</div>;
+  return (
+    <div className='nouser'>
+      <div className='nouser__icon'>
+        <img className='nouser__icon_img' src='./assets/user.svg' alt='user' />
+      </div>
+      <div className='nouser__text'>User not found</div>
+    </div>
+  );
 };
